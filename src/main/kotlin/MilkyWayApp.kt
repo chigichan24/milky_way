85b9063f62c7c19a8058ebcf4280eafaf8b38246
Kazuki Chigita <kazuki.chigichan@gmail.com>
2025-07-11 00:30:14 +0900

    feat: Adjust window size and position
    The window is now set to occupy the bottom half of the screen, with full screen width and half screen height. This change removes the maximized window placement.

--- src/main/kotlin/MilkyWayApp.kt
+++ src/main/kotlin/MilkyWayApp.kt
@@ -1,3 +1,5 @@
+import java.awt.Dimension
+import java.awt.Toolkit
 import androidx.compose.foundation.border
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -5,7 +7,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
@@ -20,7 +21,17 @@ fun MilkyWayApp(windowState: WindowState) {
 }
 
 fun main() = application {
-    val state = rememberWindowState(placement = WindowPlacement.Maximized)
+    val screenSize: Dimension = Toolkit.getDefaultToolkit().getScreenSize()
+    val screenHeight = screenSize.height
+    val screenWidth = screenSize.width
+
+    val windowHeight = screenHeight / 2
+    val windowWidth = screenWidth
+
+    val windowX = 0
+    val windowY = screenHeight / 2
+
+    val state = rememberWindowState(width = windowWidth.dp, height = windowHeight.dp, position = androidx.compose.ui.window.WindowPosition(windowX.dp, windowY.dp))
     Window(
         onCloseRequest = ::exitApplication,
         title = StringProvider.appName,
